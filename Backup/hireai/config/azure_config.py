951b16649ba8da8aa47335aaad6a99278519281a
ravindra8055 <ravindra8055@gmail.com>
2025-05-30 16:18:10 +0530

    Update azure_config.py

--- Backup/hireai/config/azure_config.py
+++ Backup/hireai/config/azure_config.py
@@ -6,5 +6,5 @@ Azure OpenAI Configuration
 AZURE_ENDPOINT = "https://openai-hackathonpoc-dev.openai.azure.com/"
 AZURE_MODEL_NAME = "gpt-35-turbo"
 AZURE_DEPLOYMENT = "hackathon-poc-turbo35"
-AZURE_SUBSCRIPTION_KEY = "d82d792194e949fe896c65340705abfe"
+AZURE_SUBSCRIPTION_KEY = "<AZURE_SUBSCRIPTION_KEY>"
 AZURE_API_VERSION = "2024-12-01-preview" 
